be9e9555ac1d333ee024f17ed9064024c64d1ea6
Peter <Z@Zamoranos.local>
2018-10-13 06:27:35 -0500

    added bandsintown API key

--- keys.js
+++ keys.js
@@ -4,3 +4,7 @@ exports.spotify = {
   id: process.env.7c880b653db24c60ae80e0337dc9b0e4,
   secret: process.env.4ca8c9eb6425462abcc7a06106995484T
 };
+
+exports.bandsintown = {
+	id: process.env.f51756bcb7d78e4ea46e194ef88431d4
+}
