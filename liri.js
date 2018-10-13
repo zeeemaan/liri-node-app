c6298c10832ea0f9afdd1ccc85863c3409d3b4e8
Peter <Z@Zamoranos.local>
2018-10-13 07:01:05 -0500

    added LIRI Bot word doc

--- liri.js
+++ liri.js
@@ -1,1 +1,7 @@
-require("dotenv").config();
+var keys = require("./keys.js").config();
+
+node liri.js concert-this <artist/band name here>
+This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+Name of the venue
+Venue location
+Date of the Event this as "MM/DD/YYYY"
